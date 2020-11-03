c7fa6bd7156ea58f622aac475e8a4d2208054279
Bilge Mutlu <bmutlu@wisc.edu>
2020-11-02 20:55:03 -0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import AsyncStorageExample from './async_storage_example.js'
+
+const App = () => {
+   return (
+      <AsyncStorageExample />
+   )
+}
+export default App
